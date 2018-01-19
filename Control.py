075b1aa23449cb5866a678a10b825e7b9eab977b
lester <lianzizhen@gmail.com>
2018-01-19 10:49:38 -0500

    L003 - adjusts to interface

--- Control.py
+++ Control.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 # @Author  : Lester
 
-import POST
+#import POST
+from post import PostLogin, manager
 import global_var as gv
 
 userList = dict()
 tasks = gv.tasks
+mng = manager.Manager()
 
 
 def init(user_info):
     for info in user_info:
         userList[info[0]] = {"phone": info[0], "pwd": info[1]}
+
+    mng.register(name="login", post_proto=PostLogin.PostLogin(gv.passport_dic, 'token'))
 
 
 def add_user(phone, pwd):
@@ -20,5 +24,8 @@ def add_user(phone, pwd):
 def process():
     for user in userList:
         for command in tasks:
-            userList[user].update(POST.post(userList[user], command))
+            executor = mng.create(command)
+            executor.run()
+            print(executor.get_response_dic())
+
             print(userList[user])
