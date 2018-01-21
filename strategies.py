c0d4a8edeb7b8e453a572564173b362a4aab9ed7
lester <lianzizhen@gmail.com>
2018-01-21 18:05:06 -0500

    L006 - User, strategies; doesn't work yet

---
+++ strategies.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 1/21/18 11:49 AM
+# @Author  : Lester
+from global_var import tasks
+
+
+def get_extra_keys(command, user):
+    if command == 'getFieldInfo':
+        return {'friendId': ""}  # for test only, should be implemented in other ways
+    elif command == 'getFieldEggs':
+        re = {'fieldIdList': []}
+        for field in user.find('fields'):
+            if field['active'] == '1':
+                re['hasEgg'] = '1'
+                re['fieldIdList'] += [field['id']]
+        user.update(re)
+        return re
+    elif command == 'getHatchCount':
+        if user.find('hasEgg') == '1':
+
+    elif command == 'hatchField':
+
