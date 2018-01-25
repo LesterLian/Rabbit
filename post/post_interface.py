fc8f51066f14da4da784a31a2a86a03880920fcb
lester <lianzizhen@gmail.com>
2018-01-24 23:34:52 -0500

    L011 - patch to 009

--- post/post_interface.py
+++ post/post_interface.py
@@ -1,3 +1,43 @@
 # -*- coding: utf-8 -*-
 # @Time    : 1/24/18 9:24 PM
 # @Author  : Lester
+from collections import OrderedDict
+from abc import abstractmethod
+
+
+class PostInterface:
+    def __init__(self, user):
+        self.user = user
+        self.post_dict = OrderedDict()
+        self.post_obj = None
+        self.next_step = ''
+        self.wrong_info = ''
+        self.init_post_dict()
+        self.post()
+        print(self.next_step)
+        if self.post_obj or self.post_obj.success:
+            self.success()
+            if self.next_step == '':
+                raise ValueError('next_step needs to be set after success.')
+        else:
+            self.fail()
+            if self.next_step == '' or self.wrong_info == '':
+                raise ValueError('next_step and wrong_info needs to be set after fail')
+
+
+
+    @abstractmethod
+    def init_post_dict(self):
+        pass
+
+    @abstractmethod
+    def post(self):
+        pass
+
+    @abstractmethod
+    def success(self):
+        pass
+
+    @abstractmethod
+    def fail(self):
+        pass
--- post/post_subclasses.py
+++ post/post_subclasses.py
@@ -1,3 +1,41 @@
 # -*- coding: utf-8 -*-
 # @Time    : 1/24/18 9:38 PM
 # @Author  : Lester
+from post.post_interface import PostInterface
+from post.post import Post
+
+
+class Login(PostInterface):
+
+    def __init__(self, user):
+        super(Login, self).__init__(user)
+
+    def post(self):
+        self.post_obj = Post('login', self.post_dict, 'userId', 'token')
+
+    def init_post_dict(self):
+        self.post_dict['phone'] = self.user.data.get('phone')
+        self.post_dict['pwd'] = self.user.data.get('pwd')
+
+    def success(self):
+        response_dic = self.post_obj.get_response_dic()
+        self.user.update(response_dic)
+        self.next_step = 'end'
+
+    def fail(self):
+        self.wrong_info = '登录'
+        self.next_step = 'end'
+
+class GetRatio(PostInterface):
+
+    def init_post_dict(self):
+        pass
+
+    def post(self):
+        pass
+
+    def success(self):
+        pass
+
+    def fail(self):
+        pass
