c9a1e4e2e76d77201e42f6a6ae655e079b198d9d
LesterLian <lianzizhen@gmailcom>
2018-02-06 21:55:00 -0500

    L023 - pop-up window fix

--- gui_popup.py
+++ gui_popup.py
@@ -7,29 +7,39 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QDialog
 
-class Ui_Dialog(object):
-    def __init__(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 160)
-        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+class Ui_Dialog(QDialog):
+    def __init__(self, parent=None):
+        super(Ui_Dialog, self).__init__(parent)
+        self.setObjectName("Dialog")
+        self.resize(400, 160)
+        self.buttonBox = QtWidgets.QDialogButtonBox(self)
         self.buttonBox.setGeometry(QtCore.QRect(150, 120, 231, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
-        self.lineEdit = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit.setGeometry(QtCore.QRect(20, 20, 361, 31))
+        self.lineEdit = QtWidgets.QLineEdit(self)
+        self.lineEdit.setGeometry(QtCore.QRect(60, 20, 320, 31))
         self.lineEdit.setObjectName("lineEdit")
-        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit_2.setGeometry(QtCore.QRect(20, 70, 361, 31))
+        self.lineEdit_2 = QtWidgets.QLineEdit(self)
+        self.lineEdit_2.setGeometry(QtCore.QRect(60, 70, 320, 31))
         self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label = QtWidgets.QLabel(self)
+        self.label.setGeometry(QtCore.QRect(20, 20, 41, 19))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self)
+        self.label_2.setGeometry(QtCore.QRect(20, 70, 41, 19))
+        self.label_2.setObjectName("label_2")
 
-        self.retranslateUi(Dialog)
-        self.buttonBox.accepted.connect(Dialog.accept)
-        self.buttonBox.rejected.connect(Dialog.reject)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi()
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+        QtCore.QMetaObject.connectSlotsByName(self)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.setWindowTitle(_translate("Dialog", "添加"))
+        self.label.setText(_translate("Dialog", "账号"))
+        self.label_2.setText(_translate("Dialog", "密码"))
 
