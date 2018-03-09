1d4cb6174db74046c8a63457e0c945391947b702
lester <lianzizhen@gmail.com>
2018-03-09 12:44:02 -0500

    L027 - altered usage of user.data; remove usage of data['id']; fix log behavior; run_button now takes parameter

--- log.py
+++ log.py
@@ -6,11 +6,22 @@ from post.post import Post
 
 
 class Log:
-    def __init__(self, path):
-        self.path_to_log = path or 'log'
-        self.file = open(self.path_to_log, 'w', encoding='utf8')
+    path_to_log = 'log'
+    file = open(path_to_log, 'w', encoding='utf8')
 
-    def log(self, message):
+    @staticmethod
+    def create(path):
+        Log.path_to_log = path or 'log'
+        Log.file.close()
+        Log.file = open(Log.path_to_log, 'w', encoding='utf8')
+
+    @staticmethod
+    def open():
+        Log.file.close()
+        Log.file = open(Log.path_to_log, 'a', encoding='utf8')
+
+    @staticmethod
+    def log(message):
         timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.now())
         text = timestamp + '    '
 
@@ -21,12 +32,50 @@ class Log:
         elif message.__class__ is str:
             text += message
         print(text)
-        self.file.write(text + '\n')
+        Log.file.write(text + '\n')
 
-    def close(self):
-        self.file.close()
+    @staticmethod
+    def flush():
+        Log.file.flush()
 
-    def clear(self):
-        self.file.close()
-        open(self.path_to_log, 'w', encoding='utf8').close()
-        self.file = open(self.path_to_log, 'a', encoding='utf8')
+    @staticmethod
+    def close():
+        Log.file.close()
+
+    @staticmethod
+    def clear():
+        Log.create()
+        Log.open()
+
+    # deprecated
+    # def __init__(self, path):
+    #     self.path_to_log = path or 'log'
+    #     self.file = open(self.path_to_log, 'w', encoding='utf8')
+    #
+    # def open(self):
+    #     self.file.close()
+    #     self.file = open(self.path_to_log, 'a', encoding='utf8')
+    #
+    # def log(self, message):
+    #     timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.now())
+    #     text = timestamp + '    '
+    #
+    #     if message is None:
+    #         text += 'POST has no response.'
+    #     elif message.__class__ is Post:
+    #         text += message.response_json['message']
+    #     elif message.__class__ is str:
+    #         text += message
+    #     print(text)
+    #     self.file.write(text + '\n')
+    #
+    # def flush(self):
+    #     self.file.flush()
+    #
+    # def close(self):
+    #     self.file.close()
+    #
+    # def clear(self):
+    #     self.file.close()
+    #     open(self.path_to_log, 'w', encoding='utf8').close()
+    #     self.file = open(self.path_to_log, 'a', encoding='utf8')
