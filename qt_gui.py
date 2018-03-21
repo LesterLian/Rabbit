f709be5ef64dd1f067c33012d73d50e385c0ed89
lester <lianzizhen@gmail.com>
2018-03-21 18:15:35 -0400

    L035 - Rename thread to mythread; adjusts method design of Browser

--- qt_gui.py
+++ qt_gui.py
@@ -9,7 +9,7 @@ from ui import Ui_MainWindow
 from user import User
 from director import Director
 from webkit import Browser
-from thread import MyThread
+from mythread import MyThread
 from time import sleep
 
 
